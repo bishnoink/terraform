e076a7749c9919d3568c3e50423f93178b54f829
Naresh Kumar <naresh.kumat@indiqus.com>
2018-03-18 22:08:45 +0530

    change compute.tf for provisioning single VM

--- compute.tf
+++ compute.tf
@@ -1,10 +1,5 @@
-variable "count" {
-  default = 2
-}
-
 resource "openstack_compute_instance_v2" "web" {
-  count = "${var.count}"
-  name = "${format("web-%02d", count.index+1)}"
+  name = "webserver1"
   image_name = "CentOS6"
   flavor_id = 2
   key_pair = "naresh"
